67383cdb8a0dc522bf767163a391f6cac8acc776
Peter Berrill <peter.berrill@tu-berlin.de>
2023-10-21 23:38:49 +0200

    update density calc for Madrid

--- feature_engineering/density_gis_madrid.py
+++ feature_engineering/density_gis_madrid.py
@@ -1,5 +1,5 @@
 # script to calculate population density by spatial unit in Madrid
-# last update Peter Berrill June 6 2023
+# last update Peter Berrill Oct 21 2023
 
 ## load in required packages ####
 import pandas as pd
@@ -38,14 +38,14 @@ gdf2.to_crs(crs0,inplace=True)
 gdf2['Municipality']= [elem.split('-')[0] for elem in gdf2.ZT208]
 gdf2['Municipality']=gdf2['Municipality'].map(lambda x: x.replace('+d','ó').replace('+s','é').replace('+r','ó'))
 
-gdf['area']=gdf.area*1e-6 # in km2
-gdf2['area']=gdf2.area*1e-6
+gdf['gu_hires_area']=gdf.area*1e-6 # in km2
+gdf2['geo_unit_area']=gdf2.area*1e-6
 
 gdf.rename(columns={'ZT1259':'geo_unit_highres'},inplace=True)
 gdf2.rename(columns={'CD_ZT208':'geo_unit'},inplace=True)
 
-df=pd.DataFrame(gdf.drop(columns='geometry'))
-df2=pd.DataFrame(gdf2.drop(columns='geometry'))
+# df=pd.DataFrame(gdf.drop(columns='geometry'))
+# df2=pd.DataFrame(gdf2.drop(columns='geometry'))
 
 # load in census section shapefiles,
 # from here, for 2018 https://www.ine.es/ss/Satellite?c=Page&p=1259952026632&pagename=ProductosYServicios%2FPYSLayout&cid=1259952026632&L=1
@@ -64,103 +64,54 @@ pop_cs_2018.rename(columns={'Sección':'CUSEC','Total':'Population'},inplace=Tru
 
 print('total population census sections is ', + pop_cs_2018.loc[pop_cs_2018['CUSEC']!='TOTAL','Population'].sum())
 
-# set the center of the census sections as the shapefile geometry 
-sec_gdf['center']=sec_gdf.centroid
-sec_gdf.rename(columns={'geometry':'polygon_sec'},inplace=True)
-sec_gdf.set_geometry('center',inplace=True)
-sec_df=pd.DataFrame(sec_gdf.drop(columns=['polygon_sec','center']))
+gdf4=gdf.overlay(gdf2,how='intersection')
+# in all cases i've seen in gdf4, only one of the gu makes up effectively all of the gu_hires area, i.e. they are identical. 
+# gu_hires 018-001 is a special case bc it is a multipolygon which doesn't contain its own centroid. 
+# therefore doing a spatial join based on centroids will incorrectly allocate 018-001 to gu 164, rather than its correct match gu 163.
+gdf4['geo_unit']=gdf4['geo_unit'].astype(int).astype(str)
+gdf4['area_over']=gdf4.area
 
-# spatial join the highres survey shapefile with census sections population data 
+# this is a valid way to get the direct 1-1 matching of high-res to low-res spatial units
+gdf5=gdf4.sort_values(by='area_over',ascending=False)
+gdf5.drop_duplicates(subset='geo_unit_highres',keep='first',inplace=True)
+# the close area match shows that the dropped rows are insignificant negligible matches prob due to inadvertent geom overlaps
+print('area included in gdf used for concordances of lo:hi res geounits',gdf5['area_over'].sum()/gdf4['area_over'].sum())
 
-# join the section dataframe and the low-res sector (ZT208) shapefiles
-join3=gpd.sjoin(sec_gdf,gdf2,how='left',predicate='within')
+# use this gdf to create a concordance df bw low and highres 
+conc=gdf5.loc[:,['geo_unit','geo_unit_highres','CD_ZT1259']]
+conc['geo_unit']=conc['geo_unit'].astype(int)
+conc.sort_values(by=['geo_unit','geo_unit_highres'],inplace=True,ascending=True)
+# merge conc into the highres gdf
+gdf=gdf.merge(conc)
 
-sec_sect=join3[['geo_unit','CUSEC']].drop_duplicates()
+# merge the pop data into the census section df
+sec_gdf_pop=sec_gdf.loc[:,['CUSEC','NMUN','geometry']].merge(pop_cs_2018)
+sec_gdf_pop['sec_area']=sec_gdf_pop.area*1e-6
 
-if sec_sect['CUSEC'].value_counts().max()>1:
-    print('census section mapped to more than one ZT208 zone')
-    sys.exit()
+# create overlay bw high-res gdf and census sections and population
+over_hi = gdf.overlay(sec_gdf_pop, how='intersection')
+over_hi['overhi_area']=over_hi.area*1e-6
+over_hi['sec_density']=over_hi['Population']/over_hi['sec_area']
+over_hi['area_share']=over_hi['overhi_area']/over_hi['sec_area']
+over_hi['Pop_calc']=over_hi['area_share']*over_hi['Population']
 
-# add in population by census section
-# merge population with the low-res sectors and census section mapping
-sec_sect_pop=sec_sect.merge(pop_cs_2018)
-# calculate total population by low-res survey sector
-sec_pop=sec_sect_pop.groupby('geo_unit')['Population'].sum().to_frame().reset_index()
+over_hi_sums=over_hi.groupby('geo_unit_highres')['Pop_calc'].sum().to_frame().reset_index()
+over_hi_sums=gpd.GeoDataFrame(over_hi_sums.merge(gdf.loc[:,['CD_ZT1259','geo_unit_highres','geo_unit','gu_hires_area','geometry']]))
+over_hi_sums['Density']=over_hi_sums['Pop_calc']/over_hi_sums['gu_hires_area']
+over_hi_sums.sort_values(by='CD_ZT1259',inplace=True)
 
-# mergre the sum populations into the low-res gdf2
-gdf2=gdf2.merge(sec_pop)
+# calculate population per low-res unit by summing the high-res
+over_lo_sums=over_hi_sums.groupby('geo_unit')['Pop_calc'].sum().reset_index()
+over_lo_sums.rename(columns={'Pop_calc':'Pop_calc_lo_sum'},inplace=True)
+over_lo_sums=gpd.GeoDataFrame(over_lo_sums.merge(gdf2.loc[:,['geo_unit','geo_unit_area','Municipality','geometry']]))
+over_lo_sums['Density']=over_lo_sums['Pop_calc_lo_sum']/over_lo_sums['geo_unit_area']
 
-# # then calculate density
-gdf2['Density']=gdf2['Population']/gdf2['area']
-print('total population in the 208 sectors ', + gdf2['Population'].sum())
+gdf_lo=over_lo_sums.copy()
+gdf_hi=over_hi_sums.copy()
 
-###
+gdf_lo.rename(columns={'Pop_calc_lo_sum':'Population','geo_unit_area':'area'},inplace=True)
+gdf_hi.rename(columns={'Pop_calc':'Population','gu_hires_area':'area'},inplace=True)
 
-# join the section dataframe and the -res (ZT1259) shapefiles
-join4=gpd.sjoin(sec_gdf,gdf,how='left',predicate='within')
-
-zone_sect=join4[['geo_unit_highres','CUSEC']].drop_duplicates()
-
-if zone_sect['CUSEC'].value_counts().max()>1:
-    print('census section mapped to more than one ZT1259 zone')
-    sys.exit()
-
-# merge population with the high-res zones and census section mapping
-zone_sect_pop=zone_sect.merge(pop_cs_2018)
-# calculate total population by low-res survey sector
-zone_pop=zone_sect_pop.groupby('geo_unit_highres')['Population'].sum().to_frame().reset_index()
-
-# geo concordances
-sec_sect=join3[['geo_unit','CUSEC']].drop_duplicates().dropna()
-zone_sect=join4[['CD_ZT1259','geo_unit_highres','CUSEC']].drop_duplicates().dropna()
-zone_sec_sect=zone_sect.merge(sec_sect,on='CUSEC')
-
-# calculate population density in the high res survey zones
-sec_gdf['area']=sec_gdf['polygon_sec'].area*1e-6
-sec_gdf_pop=sec_gdf.merge(pop_cs_2018)
-sec_gdf_pop['Density']=round(sec_gdf_pop['Population']/sec_gdf_pop['area'])
-sec_gdf_pop.set_geometry('polygon_sec',inplace=True)
-
-gdf0=gdf.copy()
-gdf0.rename(columns={'geometry':'polygon_1259'},inplace=True)
-gdf0['center']=gdf0['polygon_1259'].centroid
-gdf0.set_geometry('center',inplace=True)
-
-sec_gdf_pop0=sec_gdf_pop.loc[:,('CUSEC','NMUN','polygon_sec','area','Population','Density')]
-sec_gdf_pop0['center']=sec_gdf_pop0.centroid
-sec_gdf_pop0.set_geometry('center',inplace=True)
-
-gdfj=gpd.sjoin(gdf0.loc[:,('CD_ZT1259', 'geo_unit_highres','polygon_1259', 'area', 'center')],sec_gdf_pop.loc[:,('CUSEC','NMUN','polygon_sec','area','Population','Density')],how='left',predicate='within')
-gdfj.set_geometry('polygon_1259',inplace=True)
-dfj=pd.DataFrame(gdfj.drop(columns=['center','polygon_1259']))
-gdfj2=gpd.sjoin(sec_gdf_pop0,gdf.loc[:,('CD_ZT1259', 'geo_unit_highres','geometry', 'area')],how='left',predicate='within')
-gdfj2.set_geometry('polygon_sec',inplace=True)
-dfj2=pd.DataFrame(gdfj2.drop(columns=['center','polygon_sec']))
-
-# see which high res zones were missed out due to not having a census section centroid within their geometry
-miss=gdfj.loc[~gdfj['CD_ZT1259'].isin(gdfj2['CD_ZT1259']),'CD_ZT1259'].sort_values().reset_index(drop=True)
-gdf_miss=gdf.loc[:,('CD_ZT1259','geo_unit_highres','geometry','area')].merge(gdfj.loc[gdfj['CD_ZT1259'].isin(miss),('CD_ZT1259','geo_unit_highres','Density')].sort_values(by='CD_ZT1259')).sort_values(by='CD_ZT1259')
-
-zone_pop=gdfj2.groupby('CD_ZT1259')['Population'].sum().to_frame().reset_index()
-gdf_hi=gdf.loc[:,('CD_ZT1259','geo_unit_highres','geometry','area')].merge(zone_pop).sort_values(by='CD_ZT1259').reset_index(drop=True)
-gdf_hi['Density']=round(gdf_hi['Population']/gdf_hi['area'])
-gdf_hi.drop(columns='Population',inplace=True)
-gdf_hi=pd.concat([gdf_hi,gdf_miss]).sort_values(by='CD_ZT1259').reset_index(drop=True)
-
-# get the concordance bw high and low res geounits
-conc=gpd.sjoin(gdf0.loc[:,('CD_ZT1259', 'geo_unit_highres', 'center')],gdf2.loc[:,('geo_unit','geometry')],how='left',predicate='within')
-conc=conc.loc[:,('geo_unit','geo_unit_highres','CD_ZT1259')].drop_duplicates().sort_values(by=['geo_unit','geo_unit_highres']).reset_index(drop=True)
-# add in the multipolygon high-res unti '810' which is incorrectly allocated to geo_unit 73 (which is not included in our city definition) instead of geo_unit 106
-conc.loc[conc['CD_ZT1259']==810,'geo_unit']=106.0
-
-# merge in the low res geo_unit to allow downselecting later.
-gdf_hi=gdf_hi.merge(conc)
-gdf_hi=gdf_hi.loc[:,('CD_ZT1259', 'geo_unit_highres','geo_unit', 'geometry', 'area', 'Density',)]
-
-gdf2['Density']=gdf2['Population']/gdf2['area']
-gdf_lo=gdf2.loc[:,('geo_unit','Municipality', 'geometry', 'area', 'Density','Population')]
-
-# downselect to desired municipalities, https://en.wikipedia.org/wiki/Madrid_metropolitan_area
 ring=['Madrid','Alcorcón', 'Leganés', 'Getafe', 'Móstoles', 'Fuenlabrada', 'Coslada', 'Alcobendas', 'Pozuelo de Alarcon', 'San Fernando de Henares', # inner ring
 'Torrejon de Ardoz','Parla','Alcala de Henares','Las Rozas de Madrid','San Sebastian de los Reyes','Rivas', 'Majadahonda'] # additional municips
 
@@ -169,23 +120,21 @@ gdf_hi=gdf_hi.loc[gdf_hi['geo_unit'].isin(gdf_lo['geo_unit']),:]
 
 # now make the mixed res gdf
 size_thresh=10
-large=gdf2.loc[gdf2['area']>size_thresh,'geo_unit']
+large=gdf_lo.loc[gdf_lo['area']>size_thresh,'geo_unit']
 
 sub=gdf_hi.loc[gdf_hi['geo_unit'].isin(large),:]
-sub=sub.loc[:,('geo_unit_highres','geometry','area','Density')]
+sub=sub.loc[:,('geo_unit_highres','geometry','area','Population','Density')]
 sub.rename(columns={'geo_unit_highres':'geocode'},inplace=True)
 
-gdf_mix=gdf_lo.loc[~gdf_lo['geo_unit'].isin(large),('geo_unit','geometry','area','Density')]
+gdf_mix=gdf_lo.loc[~gdf_lo['geo_unit'].isin(large),('geo_unit','geometry','area','Population','Density')]
 gdf_mix.rename(columns={'geo_unit':'geocode'},inplace=True)
 gdf_mix['geocode']=gdf_mix['geocode'].astype(int).astype(str)
-
-print('area distribution of retained large geo-units, ')
-print(gdf_mix['area'].describe())
-print('area distribution of smaller geo-units, ')
-print(sub['area'].describe())
-# make the concatenated gdf
 gdf_mix=gpd.GeoDataFrame(pd.concat([gdf_mix,sub], ignore_index=True))
-
+print('Populations represented in low-res, high-res, and mixres density files, and original data:')
+print(gdf_lo['Population'].sum())
+print(gdf_hi['Population'].sum())
+print(gdf_mix['Population'].sum())
+print(sec_gdf_pop['Population'].sum())
 # plot each gdf and save the plot
 
 fig, ax = plt.subplots(figsize=(10, 10))
